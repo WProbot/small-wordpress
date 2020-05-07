1f2c08079e6f86bb950895018aee424fcebc8c5a
Lukács József <wprobot@loero.net>
2020-05-07 17:06:32 +0200

    Create normal.php

---
+++ wp-config/normal.php
@@ -0,0 +1,11 @@
+
+  
+  define( 'WP_MEMORY_LIMIT',     '256M'  );
+  define( 'WP_MAX_MEMORY_LIMIT', '512M'  );
+
+  define( 'CONCATENATE_SCRIPTS', false   );
+  define( 'WP_POST_REVISIONS',   '10'    );
+  define( 'MEDIA_TRASH',         true    );
+  define( 'EMPTY_TRASH_DAYS',    '15'    );
+  define( 'WP_AUTO_UPDATE_CORE', 'minor' );
+  
