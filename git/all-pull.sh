812ac0f87bff6be833495a4c01332ba8a91f98eb
Lukács József <wprobot@loero.net>
2020-05-10 22:15:05 +0200

    Create all-pull.sh

---
+++ git/all-pull.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+  cd
+
+  find . | gawk '
+
+      /[/][.]git$/ && !_[$0]++ {
+        gsub("[/][.]git$", "");
+        print "echo === $(date +%Y%n%d-%H%M%S)", $0;
+        print "cd", $0;
+        print "git pull";
+        print "cd";
+      }
+
+  ' | bash | tee -a pull.log
+
+#-#
