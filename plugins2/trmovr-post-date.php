177ad24ef3703a7e4a8ca2a72332bdcb6f8ffdfb
Lukács József <wprobot@loero.net>
2021-07-06 21:59:49 +0200

    Update trmovr-post-date.php

--- plugins2/trmovr-post-date.php
+++ plugins2/trmovr-post-date.php
@@ -2,8 +2,11 @@
 
 
   function fixer_remove_post_dates() {
-    add_filter('the_date', '__return_false  add_filter('the_time', '__return_false');
-    add_filter('the_modified_date', '__return_false');
+    if( ! is_admin() ) {
+      add_filter('the_date', '__return_false');  
+      add_filter('the_time', '__return_false');
+      add_filter('the_modified_date', '__return_false');
+    }
   } 
 
   add_action('loop_start', 'fixer_remove_post_dates');
